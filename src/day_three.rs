b81b58682bd09cee66c17775c48a32e20962de6a
Chantry Cargill <chantry.cargill@skybettingandgaming.com>
2020-12-03 10:10:29 +0000

    Day 3 Part 2

--- src/day_three.rs
+++ src/day_three.rs
@@ -7,7 +7,12 @@ struct Position {
     count: usize
 }
 
-fn calculate_collisions(board_size: usize, iterator: impl Iterator<Item = String>) -> Position {
+fn calculate_collisions<'a>(
+    board_size: usize,
+    horizontal_skip: usize,
+    vertical_skip: usize,
+    iterator: impl Iterator<Item = &'a String>
+) -> Position {
     iterator.map(|line| line
         .chars()
         .enumerate()
@@ -17,22 +22,50 @@ fn calculate_collisions(board_size: usize, iterator: impl Iterator<Item = String
         })
         .collect::<Vec<usize>>()
     )
-        .fold(Position { horizontal: 0, count: 0 }, |position, tree_positions| {
-            Position {
-                horizontal: (position.horizontal + 3) % board_size,
-                count: position.count + if tree_positions.contains(&position.horizontal) {
-                    1
-                } else {
-                    0
+        .enumerate()
+        .fold(
+            Position { horizontal: 0, count: 0 },
+            |position, (index, tree_positions)| {
+                if index % vertical_skip != 0 {
+                    return position
+                }
+
+                Position {
+                    horizontal: (position.horizontal + horizontal_skip) % board_size,
+                    count: position.count + if tree_positions.contains(&position.horizontal) {
+                        1
+                    } else {
+                        0
+                    }
                 }
             }
-        })
+        )
 }
 
 #[allow(dead_code)]
 pub fn run_day_three() {
-    let result = calculate_collisions(BOARD_SIZE, read_lines("assets/day_three"));
-    println!("Result {}", result.count)
+    let lines = read_lines("assets/day_three").collect::<Vec<String>>();
+    let result = calculate_collisions(
+        BOARD_SIZE,
+        3,
+        1,
+        lines.iter()
+    );
+    println!("Result Task 1 {}", result.count);
+
+    let second_result: usize = [[1, 1], [1, 5], [1, 7], [2, 1]]
+        .iter()
+        .map(|slope|
+            calculate_collisions(
+                BOARD_SIZE,
+                slope[1],
+                slope[0],
+                lines.iter()
+            ).count
+        )
+        .product();
+
+    println!("Result Task 2 {}", second_result * result.count)
 }
 
 #[cfg(test)]
@@ -40,20 +73,32 @@ mod tests {
     use crate::day_three::*;
 
     #[test]
-    fn should_calculate_collisions() {
-        let result = calculate_collisions(11, vec!(
-            String::from("..##......."),
-            String::from("#...#...#.."),
-            String::from(".#....#..#."),
-            String::from("..#.#...#.#"),
-            String::from(".#...##..#."),
-            String::from("..#.##....."),
-            String::from(".#.#.#....#"),
-            String::from(".#........#"),
-            String::from("#.##...#..."),
-            String::from("#...##....#"),
-            String::from(".#..#...#.#")
+    fn should_calculate_collisions_with_horizontal_skip() {
+        let result = calculate_collisions(11, 3, 1, vec!(
+            &String::from("..##......."),
+            &String::from("#...#...#.."),
+            &String::from(".#....#..#."),
+            &String::from("..#.#...#.#"),
+            &String::from(".#...##..#."),
+            &String::from("..#.##....."),
+            &String::from(".#.#.#....#"),
+            &String::from(".#........#"),
+            &String::from("#.##...#..."),
+            &String::from("#...##....#"),
+            &String::from(".#..#...#.#")
         ).into_iter());
         assert_eq!(result.count, 7)
+    }
+
+    #[test]
+    fn should_calculate_collisions_with_vertical_skip() {
+        let result = calculate_collisions(11, 1, 2, vec!(
+            &String::from("..##......."),
+            &String::from("....#...#.."),
+            &String::from(".#....#..#."),
+            &String::from("..#........"),
+            &String::from("..#........")
+        ).into_iter());
+        assert_eq!(result.count, 2)
     }
 }
