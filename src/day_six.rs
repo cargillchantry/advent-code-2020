2dbca12b6265b72c3a6c88c517c72a518e07d25d
Chantry Cargill <chantry.cargill@skybettingandgaming.com>
2020-12-05 17:02:33 +0000

    Prepared for day 6

---
+++ src/day_six.rs
@@ -0,0 +1,19 @@
+use crate::file_util::read_lines;
+
+
+#[allow(dead_code)]
+pub fn run_day_six() {
+    let _iterator = read_lines("assets/day_six");
+
+    println!("Result: {} {}", 0, 0);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_six::*;
+
+    #[test]
+    fn should_() {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::day_five::run_day_five;
+use crate::day_six::run_day_six;
 
 mod file_util;
 mod day_one;
@@ -6,8 +6,9 @@ mod day_two;
 mod day_three;
 mod day_four;
 mod day_five;
+mod day_six;
 
 fn main() {
-    run_day_five()
+    run_day_six()
 }
 
