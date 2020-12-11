5ba8ca1e25a25a3f0043e47208355e4adc2db007
Chantry Cargill <chantry.cargill@skybettingandgaming.com>
2020-12-11 18:00:49 +0000

    Small refactor. Day eleven is still a mess

--- src/day_eleven.rs
+++ src/day_eleven.rs
@@ -1,7 +1,7 @@
 use crate::file_util::read_non_blank_lines;
 use crate::day_eleven::Seat::{TAKEN, EMPTY, FLOOR};
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 enum Seat {
     TAKEN, EMPTY, FLOOR
 }
@@ -33,46 +33,42 @@ pub fn run_day_eleven() {
             _ => FLOOR
         })
         .collect::<Vec<Seat>>();
+    let mut seats_part_2 = seats.clone();
 
     let mut revisions = 0;
-    while revise_seats_part_2(row_width, &mut seats) {
+    while revise_seats(row_width, 3, &mut seats, get_adjacent_seats) {
         revisions += 1;
     }
     println!(
-        "Seat revisions {} after {} revisions",
+        "Part 1 seat revisions {} after {} revisions",
          seats.iter().filter(|x| **x == TAKEN).count(),
          revisions
+    );
+
+    revisions = 0;
+    while revise_seats(row_width, 4, &mut seats_part_2, get_non_floor_adjacent_seats) {
+        revisions += 1;
+    }
+    println!(
+        "Part 2 seat revisions {} after {} revisions",
+        seats_part_2.iter().filter(|x| **x == TAKEN).count(),
+        revisions
     )
 }
 
-#[allow(dead_code)]
-fn revise_seats(row_width: usize, seats: &mut[Seat]) -> bool {
+fn revise_seats(
+    row_width: usize,
+    max_taken: usize,
+    seats: &mut[Seat],
+    adjacent_seat_resolver: impl Fn(usize, usize, &[Seat]) -> [Option<&Seat>; 8]
+) -> bool {
     let mut changes = Vec::new();
     for index in 0..seats.len() {
         if seats[index] == EMPTY
-            && !is_any_taken(&get_adjacent_seats(index, row_width, &seats)){
+            && !is_any_taken(&adjacent_seat_resolver(index, row_width, &seats)){
             changes.push((index, TAKEN));
         } else if seats[index] == TAKEN
-            && number_taken(&get_adjacent_seats(index, row_width, &seats)) > 3 {
-            changes.push((index, EMPTY));
-        }
-    }
-
-    let changed = !changes.is_empty();
-    for (index, change) in changes.into_iter() {
-        seats[index] = change;
-    }
-    changed
-}
-
-fn revise_seats_part_2(row_width: usize, seats: &mut[Seat]) -> bool {
-    let mut changes = Vec::new();
-    for index in 0..seats.len() {
-        if seats[index] == EMPTY
-            && !is_any_taken(&get_non_floor_adjacent_seats(index, row_width, &seats)){
-            changes.push((index, TAKEN));
-        } else if seats[index] == TAKEN
-            && number_taken(&get_non_floor_adjacent_seats(index, row_width, &seats)) > 4 {
+            && number_taken(&adjacent_seat_resolver(index, row_width, &seats)) > max_taken {
             changes.push((index, EMPTY));
         }
     }
@@ -106,8 +102,8 @@ fn is_any_taken(seats: &[Option<&Seat>]) -> bool {
         )
 }
 
-fn find_non_floor_seat(mut index: isize, seats: &[Seat], direction: impl Fn(isize) -> Option<isize>) -> Option<&Seat> {
-    while let Some(seat_location) = direction(index) {
+fn find_non_floor_seat(mut index: isize, row_width: isize, seats: &[Seat], direction: impl Fn(isize, isize) -> Option<isize>) -> Option<&Seat> {
+    while let Some(seat_location) = direction(index, row_width) {
         let seat = seats.safe_get(seat_location);
         if match seat { Some(x) => *x != FLOOR, None => true } {
             return seat
@@ -125,44 +121,70 @@ fn on_right(index: isize, row_width: isize) -> bool {
     (index + 1) % row_width == 0
 }
 
+fn get_top_left_index(i: isize, row_width: isize) -> Option<isize> {
+    if on_left(i, row_width) { None } else { Some(i - row_width - 1) }
+}
+
+fn get_top_right_index(i: isize, row_width: isize) -> Option<isize> {
+    if on_right(i, row_width) { None } else { Some(i - row_width + 1) }
+}
+
+fn get_top_middle_index(i: isize, row_width: isize) -> Option<isize> {
+    Some(i - row_width)
+}
+
+fn get_left_index(i: isize, row_width: isize) -> Option<isize> {
+    if on_left(i, row_width) { None } else { Some(i - 1) }
+}
+
+fn get_right_index(i: isize, row_width: isize) -> Option<isize> {
+    if on_right(i, row_width) { None } else { Some(i + 1) }
+}
+
+fn get_bottom_left_index(i: isize, row_width: isize) -> Option<isize> {
+    if on_left(i, row_width) { None } else { Some(i + row_width - 1) }
+}
+
+fn get_bottom_middle_index(i: isize, row_width: isize) -> Option<isize> {
+    Some(i + row_width)
+}
+
+fn get_bottom_right_index(i: isize, row_width: isize) -> Option<isize> {
+    if on_right(i, row_width) { None } else { Some(i + row_width + 1) }
+}
+
 fn get_non_floor_adjacent_seats(index: usize, row_width: usize, seats: &[Seat]) -> [Option<&Seat>; 8] {
     let index = index as isize;
     let row_width = row_width as isize;
 
-    let top_left = |i| if on_left(i, row_width) { None } else { Some(i - row_width - 1) };
-    let top = |i| Some(i - row_width);
-    let top_right = |i| if on_right(i, row_width) { None } else { Some(i - row_width + 1) };
-    let left = |i| if on_left(i, row_width) { None } else { Some(i - 1) };
-    let right = |i| if on_right(i, row_width) { None } else { Some(i + 1) };
-    let bottom_left = |i| if on_left(i, row_width) { None } else { Some(i + row_width - 1) };
-    let bottom = |i| Some(i + row_width);
-    let bottom_right = |i| if on_right(i, row_width) { None } else { Some(i + row_width + 1) };
     return [
-        find_non_floor_seat(index, &seats, top_left),
-        find_non_floor_seat(index, &seats, top),
-        find_non_floor_seat(index, &seats, top_right),
-        find_non_floor_seat(index, &seats, left),
-        find_non_floor_seat(index, &seats, right),
-        find_non_floor_seat(index, &seats, bottom_left),
-        find_non_floor_seat(index, &seats, bottom),
-        find_non_floor_seat(index, &seats, bottom_right)
+        find_non_floor_seat(index, row_width, &seats, get_top_left_index),
+        find_non_floor_seat(index, row_width, &seats, get_top_middle_index),
+        find_non_floor_seat(index, row_width, &seats, get_top_right_index),
+        find_non_floor_seat(index, row_width, &seats, get_left_index),
+        find_non_floor_seat(index, row_width, &seats, get_right_index),
+        find_non_floor_seat(index, row_width, &seats, get_bottom_left_index),
+        find_non_floor_seat(index, row_width, &seats, get_bottom_middle_index),
+        find_non_floor_seat(index, row_width, &seats, get_bottom_right_index)
     ];
+}
+
+fn retrieve(maybe_index: Option<isize>, seats: &[Seat]) -> Option<&Seat> {
+    maybe_index.and_then(|i| seats.safe_get(i))
 }
 
 fn get_adjacent_seats(index: usize, row_width: usize, seats: &[Seat]) -> [Option<&Seat>; 8] {
     let index = index as isize;
     let row_width = row_width as isize;
-    let is_on_left = on_left(index, row_width);
-    let is_on_right = on_right(index, row_width);
     return [
-        if is_on_left { None } else { seats.safe_get(index - row_width - 1) } ,
-        seats.safe_get(index - row_width),
-        if is_on_right { None } else { seats.safe_get(index - row_width + 1) },
-        if is_on_left { None } else { seats.safe_get(index - 1) },
-        if is_on_right { None } else { seats.safe_get(index + 1) },
-        if is_on_left { None } else { seats.safe_get(index + row_width - 1) },
-        seats.safe_get(index + row_width),
-        if is_on_right { None } else { seats.safe_get(index + row_width + 1) }
+        retrieve(get_top_left_index(index, row_width), &seats),
+        retrieve(get_top_middle_index(index, row_width), &seats),
+        retrieve(get_top_right_index(index, row_width), &seats),
+        retrieve(get_left_index(index, row_width), &seats),
+        retrieve(get_right_index(index, row_width), &seats),
+        retrieve(get_bottom_left_index(index, row_width), &seats),
+        retrieve(get_bottom_middle_index(index, row_width), &seats),
+        retrieve(get_bottom_right_index(index, row_width), &seats)
     ];
 }
 
