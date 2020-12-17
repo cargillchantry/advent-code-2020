94a891f180c081dd8fc5a076155ec427665a4403
Chantry Cargill <chantry.cargill@skybettingandgaming.com>
2020-12-17 21:37:02 +0000

    Garbage part 2

--- src/day_seventeen.rs
+++ src/day_seventeen.rs
@@ -1,75 +1,67 @@
 use crate::day_seventeen::Block::{Active, Inactive};
 use crate::file_util::read_non_blank_lines;
 use std::collections::HashMap;
+use std::iter::repeat;
+use itertools::Itertools;
 
 #[derive(Eq, PartialEq, Clone)]
 enum Block {
     Active, Inactive
 }
 
-type Coord = (isize, isize, isize);
-
 struct Board {
-    blocks: HashMap<Coord, Block>
+    dimensions: u32,
+    blocks: HashMap<Vec<isize>, Block>
 }
 
 impl Board {
-    fn new() -> Self {
+    fn new(dimensions: u32) -> Self {
         Board {
+            dimensions,
             blocks: HashMap::new()
         }
     }
 
-    fn neighbours(&self, coord: Coord) -> Vec<(Coord, &Block)> {
-       vec!(
-            // above
-            (coord.0 - 1, coord.1 + 1, coord.2 + 1),
-            (coord.0, coord.1 + 1, coord.2 + 1),
-            (coord.0 + 1, coord.1 + 1, coord.2 + 1),
-            (coord.0 - 1, coord.1, coord.2 + 1),
-            (coord.0, coord.1, coord.2 + 1),
-            (coord.0 + 1, coord.1, coord.2 + 1),
-            (coord.0 - 1, coord.1 - 1, coord.2 + 1),
-            (coord.0, coord.1 - 1, coord.2 + 1),
-            (coord.0 + 1, coord.1 - 1, coord.2 + 1),
-            // same
-            (coord.0 - 1, coord.1 + 1, coord.2),
-            (coord.0, coord.1 + 1, coord.2),
-            (coord.0 + 1, coord.1 + 1, coord.2),
-            (coord.0 - 1, coord.1, coord.2),
-            (coord.0 + 1, coord.1, coord.2),
-            (coord.0 - 1, coord.1 - 1, coord.2),
-            (coord.0, coord.1 - 1, coord.2),
-            (coord.0 + 1, coord.1 - 1, coord.2),
-            // below
-            (coord.0 - 1, coord.1 + 1, coord.2 - 1),
-            (coord.0, coord.1 + 1, coord.2 - 1),
-            (coord.0 + 1, coord.1 + 1, coord.2 - 1),
-            (coord.0 - 1, coord.1, coord.2 - 1),
-            (coord.0, coord.1, coord.2 - 1),
-            (coord.0 + 1, coord.1, coord.2 - 1),
-            (coord.0 - 1, coord.1 - 1, coord.2 - 1),
-            (coord.0, coord.1 - 1, coord.2 - 1),
-            (coord.0 + 1, coord.1 - 1, coord.2 - 1)
-        )
-            .into_iter()
-            .map(|x| (x, self.get_block(&x)))
+    fn neighbours(&self, coord: &Vec<isize>) -> Vec<(Vec<isize>, &Block)> {
+        let mut iterators = Vec::new();
+        for i in 0..self.dimensions {
+            iterators.push(
+                repeat(0_isize).take(3_usize.pow(i))
+                    .chain(repeat(1).take(3_usize.pow(i)))
+                    .chain(repeat(-1).take(3_usize.pow(i)))
+                    .cycle()
+            );
+        }
+        (0 .. 3_usize.pow(self.dimensions) * self.dimensions as usize)
+            .filter_map(|i| iterators[i % (self.dimensions as usize)].next())
+            .batching(|iter| {
+                let mut indexes = Vec::new();
+                for i in 0..self.dimensions {
+                    indexes.push(coord[i as usize] + iter.next()?)
+                }
+                Some(indexes)
+            })
+            .skip(1)
+            .map(|x| {
+                let block = self.get_block(&x);
+                (x, block)
+            })
             .collect()
     }
 
-    fn set_active(&mut self, coord: (isize, isize, isize)) {
-        self.blocks.insert(coord, Active);
+    fn set_active(&mut self, coord: &Vec<isize>) {
+        self.blocks.insert(coord.clone(), Active);
     }
 
-    fn set_inactive(&mut self, coord: &(isize, isize, isize)) {
-        self.blocks.remove(&coord);
+    fn set_inactive(&mut self, coord: &Vec<isize>) {
+        self.blocks.remove(coord);
     }
 
-    fn get_block(&self, coord: &(isize, isize, isize)) -> &Block {
-        self.blocks.get(&coord).unwrap_or(&Inactive)
+    fn get_block(&self, coord: &Vec<isize>) -> &Block {
+        self.blocks.get(coord).unwrap_or(&Inactive)
     }
 
-    fn get_active_blocks(&self) -> Vec<&(isize, isize, isize)> {
+    fn get_active_blocks(&self) -> Vec<&Vec<isize>> {
         self.blocks.keys().collect()
     }
 }
@@ -78,24 +70,24 @@ fn iterate_board(board: &mut Board) {
     let mut changes = Vec::new();
     let active_blocks = board.get_active_blocks();
     active_blocks.iter().for_each(|block| {
-        let neighbours = board.neighbours(**block);
+        let neighbours = board.neighbours(block);
         let active_neighbours = neighbours.iter()
             .filter(|x| *x.1 == Active)
             .count();
         if active_neighbours < 2 || active_neighbours > 3 {
-            changes.push((**block, Inactive));
+            changes.push(((*block).clone(), Inactive));
         }
         neighbours.iter()
-            .filter(|x| *x.1 == Inactive && board.neighbours(x.0)
+            .filter(|x| *x.1 == Inactive && board.neighbours(&x.0)
                 .iter()
                 .filter(|y| *y.1 == Active)
                 .count() == 3
             )
-            .for_each(|x| changes.push((x.0, Active)));
+            .for_each(|x| changes.push((x.0.clone(), Active)));
     });
-    changes.iter().for_each(|it|
+    changes.into_iter().for_each(|it|
         if it.1 == Active {
-            board.set_active(it.0);
+            board.set_active(&it.0);
         } else {
             board.set_inactive(&it.0);
         }
@@ -104,19 +96,19 @@ fn iterate_board(board: &mut Board) {
 
 #[allow(dead_code)]
 pub fn run_day_seventeen() {
-    let mut board = Board::new();
+    let mut board = Board::new(4);
     read_non_blank_lines("assets/day_seventeen")
         .enumerate()
         .for_each(|(x, line)|
             line.chars().enumerate().for_each(|(y, c)| {
                 if c == '#' {
-                    board.set_active((x as isize, y as isize, 0));
+                    board.set_active(&vec!(x as isize, y as isize, 0, 0));
                 }
             })
         );
     for _ in 0..6 {
         iterate_board(&mut board);
     }
-    let result_part_one = board.get_active_blocks().iter().count();
-    println!("Result part one {}", result_part_one);
+    let result_part_two = board.get_active_blocks().iter().count();
+    println!("Result part two {}", result_part_two);
 }
