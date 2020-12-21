26cfda41b2918038218fe905f07fce2629fe449c
Chantry Cargill <chantry.cargill@skybettingandgaming.com>
2020-12-21 11:38:43 +0000

    Refactor for task 2 day 20

--- src/day_twenty.rs
+++ src/day_twenty.rs
@@ -5,11 +5,31 @@ struct Block {
     id: u16,
     rows: [u16; 10],
     left: u16,
-    right: u16
+    right: u16,
+    matching_ids: [u16; 4]
 }
 
 impl Block {
     fn borders(&self) -> [u16; 4] { [self.rows[0], self.left, self.right, self.rows[9]] }
+    fn add_matching_sides(&mut self, block: &mut Block) -> &mut Self {
+        if self.id == block.id {
+            return self;
+        }
+        for (id, side) in self.borders().iter().enumerate() {
+            let flipped_side = side.flip_side();
+            for (other_id, other_side) in block.borders().iter().enumerate() {
+                if side == other_side || flipped_side == *other_side {
+                    self.matching_ids[id] = block.id;
+                    block.matching_ids[other_id] = self.id;
+                    return self;
+                }
+            }
+        }
+        self
+    }
+    fn missing_sides(&self) -> usize {
+        self.matching_ids.iter().filter(|x| **x != 0).count()
+    }
 }
 
 trait FlipSide {
@@ -23,25 +43,29 @@ impl FlipSide for u16 {
 #[allow(dead_code)]
 pub fn run_day_twenty() {
     let mut iter = read_lines("assets/day_twenty");
-    let signatures = read_image_signatures(&mut iter);
-    let mut corners = Vec::new();
-    for (idx, signature) in signatures.iter().enumerate() {
-        let mut count = 0;
-        for side in signature.borders().iter() {
-            let reversed = side.flip_side();
-            let matches = signatures.iter().enumerate().any(|(other_idx, x)|
-                other_idx != idx &&
-                x.borders().iter().any(|y| y == side || *y == reversed)
-            );
-            if matches {
-                count += 1;
+    let mut signatures = read_image_signatures(&mut iter);
+    populate_matches(&mut signatures);
+
+    println!(
+        "Part 1 {:?}",
+        signatures.iter()
+            .filter(|x| x.missing_sides() == 2)
+            .map(|it| it.id as usize)
+            .product::<usize>()
+    );
+}
+
+fn populate_matches(signatures: &mut Vec<Block>) {
+    for i in 1..signatures.len() {
+        let split = signatures.split_at_mut(i);
+        let left = split.0;
+        let right = split.1;
+        if let Some(signature )= left.last_mut() {
+            for other_signature in right.iter_mut(){
+                signature.add_matching_sides(other_signature);
             }
         }
-        if count < 3 {
-            corners.push(signature.id);
-        }
     }
-    println!("Part 1 {:?}", corners.iter().map(|x| *x as usize).product::<usize>());
 }
 
 fn read_image_signatures(iter: &mut impl Iterator<Item = String>) -> Vec<Block> {
@@ -84,7 +108,8 @@ fn read_image_signature(iter: &mut impl Iterator<Item = String>) -> Option<Block
             id,
             left,
             right,
-            rows
+            rows,
+            matching_ids: [0; 4]
         }
     )
 }
